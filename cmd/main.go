63390525d4d9cd5ac7e14e0bea26a905f6c7492e
medalon <medalon07@gmail.com>
2018-06-12 10:57:53 -0700

    insert, update to DB

--- cmd/main.go
+++ cmd/main.go
@@ -26,8 +26,8 @@ func main() {
 	logFile = "dummy"
 
 	if logFile == "dummy" {
-		logReader = strings.NewReader(`194.152.36.162 - - [18/Feb/2018:16:54:10 +0600] "GET /1px.png?stat=preroll&name=Beeline_PACMAN&act=click&pid=bQFrkd9E HTTP/1.1" 200 923 "https://www.super.kg/embed/video/183223?mesto=1&ismobile=1" "Mozilla/5.0 (Linux; Android 6.0.1; SM-G532F Build/MMB29T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36" "-"
-		91.192.66.244 - - [20/Feb/2018:04:22:24 +0600] "GET /1px.png?stat=preroll&name=Beeline_PACMAN&act=show&pid=SeD3e3Ra HTTP/1.1" 200 923 "https://www.super.kg/embed/video/210837?mesto=0&ismobile=1" "Mozilla/5.0 (Linux; U; Android 6.0.1; en-US; ASUS_Z010DD Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/40.0.2214.89 UCBrowser/11.5.0.1015 Mobile Safari/537.36" "-"`)
+		logReader = strings.NewReader(`1.2.3.4 - - [18/Feb/2018:16:54:10 +0600] "GET /1px.png?stat=preroll&name=PACMAN&act=click&pid=bQFrkd9E HTTP/1.1" 200 923 "https://localhost/embed/video/183223?mesto=1&ismobile=1" "Mozilla/5.0 (Linux; Android 6.0.1; SM-G532F Build/MMB29T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36" "-"
+		1.2.3.4 - - [20/Feb/2018:04:22:24 +0600] "GET /1px.png?stat=preroll&name=PACMAN&act=show&pid=SeD3e3Ra HTTP/1.1" 200 923 "https://localhost/embed/video/210837?mesto=0&ismobile=1" "Mozilla/5.0 (Linux; U; Android 6.0.1; en-US; ASUS_Z010DD Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/40.0.2214.89 UCBrowser/11.5.0.1015 Mobile Safari/537.36" "-"`)
 	} else if logFile == "-" {
 		logReader = os.Stdin
 	} else {
